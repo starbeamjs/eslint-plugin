ee5eecee64702017845b1573048a54c9c4e60fe7
Yehuda Katz <wycats@gmail.com>
2023-10-18 14:03:38 -0700

    Add a complete order for tsconfig.json

--- src/json.ts
+++ src/json.ts
@@ -138,6 +138,149 @@ export const tsconfigJSON = jsonc({
         "include",
         "exclude",
         "references",
+        "watchOptions",
+        "typeAcquisition",
+      ],
+    },
+
+    {
+      pathPattern: "^compilerOptions$",
+      order: [
+        // projects
+        "composite",
+        "disableReferencedProjectLoad",
+        "disableSolutionSearching",
+        "disableSourceOfProjectReferenceRedirect",
+        "tsBuildInfoFile",
+
+        // tsc behavior
+        "noEmitOnError",
+        "incremental",
+
+        // completeness
+        "skipLibCheck",
+        "skipDefaultLibCheck",
+
+        // target environment
+        "target",
+        "module",
+        "moduleResolution",
+
+        // runtime environment
+        "lib",
+        "moduleDetection",
+        "noLib",
+        "useDefineForClassFields",
+
+        // JS support
+        "allowJs",
+        "checkJs",
+        "maxNodeModuleJsDepth",
+
+        // extra TS-specific language features
+        "emitDecoratorMetadata",
+        "experimentalDecorators",
+        "jsx",
+        "jsxImportSource",
+        "jsxFactory",
+        "jsxFragmentFactory",
+
+        // runtime interop constraints
+        "allowSyntheticDefaultImports",
+        "esModuleInterop",
+        "isolatedModules",
+
+        // file system interop constraints
+        "forceConsistentCasingInFileNames",
+        "preserveSymlinks",
+
+        // project structure features
+        "paths",
+        "rootDir",
+        "rootDirs",
+
+        // where to get extra types
+        "types",
+        "typeRoots",
+
+        // transform options
+        "preserveConstEnums",
+        "removeComments",
+        "stripInternal",
+
+        // strict features
+        "strict",
+        // optional strict features (even with strict: true)
+        "exactOptionalPropertyTypes",
+        "noImplicitOverride",
+        "noPropertyAccessFromIndexSignature",
+        "noUncheckedIndexedAccess",
+
+        // included in strict: true
+        "alwaysStrict",
+        "strictNullChecks",
+        "strictBindCallApply",
+        "strictFunctionTypes",
+        "strictPropertyInitialization",
+        "noImplicitAny",
+        "noImplicitThis",
+        "useUnknownInCatchVariables",
+
+        // module stuff
+        "moduleSuffixes",
+        "allowArbitraryExtensions",
+        "allowImportingTsExtensions",
+        "customConditions",
+
+        // unusual module stuff
+        "noResolve",
+
+        // not recommended (on by default when using recommended features)
+        "resolveJsonModule",
+        "resolvePackageJsonImports",
+
+        // deprecated module stuff
+        "importsNotUsedAsValues",
+
+        // basic emit
+        "noEmit",
+        "importHelpers",
+
+        // source maps
+        "sourceMap",
+        "inlineSources",
+        "inlineSourceMap",
+        "sourceRoot",
+        "mapRoot",
+
+        // what and where to emit
+        "declaration",
+        "declarationDir",
+        "declarationMap",
+        "emitDeclarationOnly",
+        "outDir",
+
+        // not recommended emit
+        "downlevelIteration",
+        "emitBOM",
+        "newLine",
+        "noEmitHelpers",
+        "outFile",
+
+        // editor support
+        "plugins",
+
+        // unusual editor support
+        "disableSizeLimit",
+
+        // compiler diagnostics
+        "diagnostics",
+        "listFiles",
+        "listEmittedFiles",
+        "explainFiles",
+        "traceResolution",
+        "extendedDiagnostics",
+        "generateCpuProfile",
       ],
     },
 
